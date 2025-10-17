8db5bee78b32ff014ef692a3e5ac9294c68652b4
JakubJagodzinski <j.jagodzinski.priv@gmail.com>
2025-10-18 01:01:06 +0200

    Enable Spring Data web DTO serialization mode

--- src/main/java/com/example/schoolmoney/config/WebConfig.java
+++ src/main/java/com/example/schoolmoney/config/WebConfig.java
@@ -3,11 +3,13 @@ package com.example.schoolmoney.config;
 import com.example.schoolmoney.interceptor.RequestLoggingInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
 
     private final RequestLoggingInterceptor requestLoggingInterceptor;
