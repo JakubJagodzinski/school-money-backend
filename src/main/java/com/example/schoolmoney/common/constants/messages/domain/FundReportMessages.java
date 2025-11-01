5d00935bcdbd25cad8e122b343a878554058526a
JakubJagodzinski <j.jagodzinski.priv@gmail.com>
2025-11-01 03:39:12 +0100

    Remove unused messages constants

--- src/main/java/com/example/schoolmoney/common/constants/messages/domain/FundReportMessages.java
+++ src/main/java/com/example/schoolmoney/common/constants/messages/domain/FundReportMessages.java
@@ -3,6 +3,5 @@ package com.example.schoolmoney.common.constants.messages.domain;
 public class FundReportMessages {
 
     public static final String PDF_REPORT_GENERATION_FAILED = "Failed to generate PDF fund report";
-    public static final String FAILED_TO_ADD_FUND_LOGO_TO_REPORT = "Failed to add fund logo to report";
 
 }
